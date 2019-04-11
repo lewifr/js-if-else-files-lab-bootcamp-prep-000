bed88cf82e8c2e19db6c76dd8af39c95fcd2eec0
Francis Lewis <francis.lewis@me.com>
2019-04-11 15:21:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var height = "74"
 if (name === "Susan") {
     console.log("The name is correct")
 }
+else {
+  consol.log('Expected Susan !!!')
+}
 
 // Don't worry about this
 module.exports = { name, height
